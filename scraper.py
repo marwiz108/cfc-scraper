789a9a284022602b06877887232a9b0ed9d97e20
Marwa Elali <marwaelali@gmail.com>
2023-02-24 19:19:24 -0500

    refactor

--- scraper.py
+++ scraper.py
@@ -2,20 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import re
+import collections
 
 HOST = "https://www.cfcunderwriting.com"
 
 def is_external(url):
-    # return (tag[attr].startswith("http") or tag[attr].startswith("//")) and "cfcunderwriting" not in tag[attr]
     return (url.startswith("http") or url.startswith("//")) and "cfcunderwriting" not in url
-
-# def has_resource(tag):
-#     if (tag.has_attr("href")):
-#         return is_external(tag, "href")
-#     elif (tag.has_attr("src")):
-#         return is_external(tag, "src")
-#     else:
-#         return False
 
 def get_external_resources(all_urls):
     external_resources = []
@@ -25,13 +17,13 @@ def get_external_resources(all_urls):
 
     return external_resources
 
-def has_url(tag):
-    return tag.has_attr("href") or tag.has_attr("src")
-
 def get_url(tag, attr):
     return tag[attr]
 
-def write_to_file(filename, content):
+def has_url(tag):
+    return tag.has_attr("href") or tag.has_attr("src")
+
+def write_to_json_file(filename, content):
     with open(filename, "w") as outfile:
         json.dump(content, outfile)
     outfile.close()
@@ -41,13 +33,14 @@ def scrape_page(url):
     return BeautifulSoup(r.content, "html.parser")
 
 def main():
+    # scrape index page
     index_page = scrape_page(HOST)
     all_resources = index_page.find_all(has_url)  # full tags
     all_urls = [get_url(r, "href") if r.has_attr("href") else get_url(r, "src") for r in all_resources]  # urls
 
     # json list of external resources
     external_resources = get_external_resources(all_urls)
-    write_to_file("external_resources.json", external_resources)
+    write_to_json_file("external_resources.json", external_resources)
 
     privacy_page_i = None   # index of privacy policy page
     privacy_url = None      # url of privacy policy page
